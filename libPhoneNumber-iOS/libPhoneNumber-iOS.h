3ba53dc324e1661457395fd77bebd37ae7de73b6
Simone Manganelli <simX@mac.com>
2015-03-24 18:01:28 -0700

    add framework target for Carthage

---
+++ libPhoneNumber-iOS/libPhoneNumber-iOS.h
@@ -0,0 +1,19 @@
+//
+//  libPhoneNumber-iOS.h
+//  libPhoneNumber-iOS
+//
+//  Created by Simone Manganelli on 24/03/15.
+//  Copyright (c) 2015 ohtalk.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libPhoneNumber-iOS.
+FOUNDATION_EXPORT double libPhoneNumber_iOSVersionNumber;
+
+//! Project version string for libPhoneNumber-iOS.
+FOUNDATION_EXPORT const unsigned char libPhoneNumber_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libPhoneNumber_iOS/PublicHeader.h>
+
+
