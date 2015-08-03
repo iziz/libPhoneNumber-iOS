c5ba3c4981f90056a9a4cb099c6bd4f0b6b114c5
iziz <zen.isis@gmail.com>
2015-08-04 01:35:02 +0900

    Remove "TEST_AFTER_BUILD"

--- script/run-test.sh
+++ script/run-test.sh
@@ -2,4 +2,4 @@
 
 xctool -project libPhoneNumber.xcodeproj -scheme libPhoneNumber clean build -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 
-xctool -project libPhoneNumber.xcodeproj -scheme libPhoneNumber -sdk iphonesimulator clean build test ONLY_ACTIVE_ARCH=NO TEST_AFTER_BUILD=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES 
+xctool -project libPhoneNumber.xcodeproj -scheme libPhoneNumber -sdk iphonesimulator clean build test ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES 
