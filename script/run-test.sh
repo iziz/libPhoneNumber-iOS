3852d2f826942516a8ffb9a1804f1620a37a3502
iziz <zen.isis@gmail.com>
2015-08-04 02:17:16 +0900

    Add slather

--- script/run-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xctool -project libPhoneNumber.xcodeproj -scheme libPhoneNumber clean build -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
-
-xctool -project libPhoneNumber.xcodeproj -scheme libPhoneNumber -sdk iphonesimulator clean build test ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES 
