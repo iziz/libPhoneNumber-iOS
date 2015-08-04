fa0e4c8610a44ed4a0b610d2cdf4d8a367cd5500
Roy Marmelstein <marmelroy@gmail.com>
2015-08-04 18:56:15 +0200

    Dynamic framework targer

---
+++ libPhoneNumber-iOS/libPhoneNumberFramework.h
@@ -0,0 +1,30 @@
+//
+//  libPhoneNumber-iOS.h
+//  libPhoneNumber-iOS
+//
+//  Created by Roy Marmelstein on 04/08/2015.
+//  Copyright (c) 2015 ohtalk.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libPhoneNumber-iOS.
+FOUNDATION_EXPORT double libPhoneNumber_iOSVersionNumber;
+
+//! Project version string for libPhoneNumber-iOS.
+FOUNDATION_EXPORT const unsigned char libPhoneNumber_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libPhoneNumber_iOS/PublicHeader.h>
+
+
+#import "NBPhoneMetaDataGenerator.h"
+
+#import "NBPhoneNumberUtil.h"
+
+#import "NBMetadataHelper.h"
+#import "NBPhoneMetaData.h"
+
+#import "NBPhoneNumber.h"
+#import "NBPhoneNumberDesc.h"
+#import "NBNumberFormat.h"
+
