354a8c0bd198b20fa8d34e2dbb9c43827c70e1d6
crontab <gith139>
2016-01-31 20:41:46 +0000

    Force linkage of NBMetadataCore.m
    Useful when linking the lib files statically, or linking against a
    static libPhoneNumber

--- libPhoneNumber/NBMetadataHelper.m
+++ libPhoneNumber/NBMetadataHelper.m
@@ -8,6 +8,7 @@
 
 #import "NBMetadataHelper.h"
 #import "NBPhoneMetaData.h"
+#import "NBMetadataCore.h"
 
 
 #if TESTING==1
@@ -46,6 +47,9 @@ static NSMutableDictionary *kMapCCode2CN = nil;
  */
 - (void)initializeHelper
 {
+	if (!NBPhoneMetadataAM.class) // force linkage of NBMetadataCore.m
+		return;
+
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         kMapCCode2CN = [NSMutableDictionary dictionaryWithObjectsAndKeys:
