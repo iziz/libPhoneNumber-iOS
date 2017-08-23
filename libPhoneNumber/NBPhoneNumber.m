2ec9a350469aab39018f67e6fde652538da37294
Cory Kilger <cmkilger@gmail.com>
2017-08-23 14:39:25 -0600

    Better hash for NBPhoneNumber.
    This makes NBPhoneNumber a better key in an NSDictionary.

--- libPhoneNumber/NBPhoneNumber.m
+++ libPhoneNumber/NBPhoneNumber.m
@@ -48,8 +48,12 @@
 
 - (NSUInteger)hash
 {
-    NSData *selfObject = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [selfObject hash];
+    NSUInteger hash = 0;
+    hash ^= self.countryCode.hash + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    hash ^= self.nationalNumber.hash + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    hash ^= self.numberOfLeadingZeros.hash + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    hash ^= self.extension.hash + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+    return hash;
 }
 
 
