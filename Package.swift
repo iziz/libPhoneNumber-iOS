05f22d76462da37240ced94da639fcf0a98ead9a
Karim H <karim@karhm.com>
2020-05-01 03:46:44 +0100

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "libPhoneNumber",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "libPhoneNumber",
+            targets: ["libPhoneNumber"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libPhoneNumber",
+            path: "libPhoneNumber",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ]
+        ),
+        .testTarget(
+            name: "libPhoneNumberTests",
+            dependencies: ["libPhoneNumber"],
+            path: "libPhoneNumberTests",
+            sources: [
+                "NBAsYouTypeFormatterTest.m",
+                "NBPhoneNumberParsingPerfTest.m",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.h",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.m",
+                "NBPhoneNumberUtilTest.m",
+                "NBShortNumberInfoTest.m"
+            ]
+        )
+    ]
+)
