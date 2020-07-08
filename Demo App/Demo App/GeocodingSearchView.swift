91fc938b52c5f3b8606b0ab4fad7597625126f16
Rastaar Haghi <rastaar@google.com>
2020-07-08 16:34:58 -0700

    Added Geocoding Search Form

--- Demo App/Demo App/GeocodingSearchView.swift
+++ Demo App/Demo App/GeocodingSearchView.swift
@@ -7,10 +7,67 @@
 //
 
 import SwiftUI
+import libPhoneNumber_iOS
+import libPhoneNumberGeocoding
 
 struct GeocodingSearchView: View {
+    @State var localeSelection = 0
+    @State var phoneNumber: String = ""
+    let languages = ["Default Device Language", "Arabic", "Belarusian", "Bulgarian", "Bosnian", "German", "Greek", "English", "Spanish", "Persian", "Finnish", "French", "Croatian", "Hungarian", "Armenian", "Indonesian", "Italian", "Hebrew", "Japanese", "Korean", "Dutch", "Polish", "Portuguese", "Romanian", "Russian", "Albanian", "Serbian", "Swedish", "Thai", "Turkish", "Ukrainian", "Vietnamese", "Chinese", "Chinese (Traditional)"]
+
+    let locales = ["Default Device Language", "ar", "be", "bg", "bs", "de", "el", "en", "es", "fa", "fi",  "fr", "hr", "hu", "hy", "id", "it", "iw", "ja", "ko", "nl", "pl", "pt", "ro", "ru", "sq", "sr", "sv", "th", "tr", "uk", "vi", "zh", "zh_Hant"]
+    @State var regionDescription: String = ""
+    
+    let geocoder = NBPhoneNumberOfflineGeocoder()
+    let phoneUtil = NBPhoneNumberUtil()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                Section(header: Text("Locale Options")) {
+                    Picker("Locale Options", selection: $localeSelection) {
+                        ForEach(0 ..< languages.count) { index in
+                            Text(self.languages[index])
+                                .tag(index)
+                        }
+                    }
+                }
+                Section(header: Text("Phone Number")) {
+                    TextField("Ex: 19098611234", text: $phoneNumber)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                }
+                Button(action: {
+                    self.regionDescription = self.searchPhoneNumber()
+                }, label: {
+                    Text("Search for Region Description")
+                        .multilineTextAlignment(.center)
+                })
+            }
+            Text(regionDescription)
+                .bold()
+        }
+        .navigationBarTitle(Text("Search for Region Description"))
+                
+        
+    }
+}
+
+extension GeocodingSearchView {
+    func searchPhoneNumber() -> String {
+        do {
+            print(Locale.current.regionCode!)
+            let parsedPhoneNumber: NBPhoneNumber = try phoneUtil.parse(phoneNumber, defaultRegion: Locale.current.regionCode!)
+            print(parsedPhoneNumber)
+            if localeSelection == 0 {
+                return geocoder.description(for: parsedPhoneNumber) ?? "Unknown Region"
+            } else {
+                return geocoder.description(for: parsedPhoneNumber, withLanguageCode: locales[localeSelection]) ?? "Unknown Region"
+            }
+        } catch let error {
+            print(error)
+            return "Error parsing phone number."
+        }
     }
 }
 
