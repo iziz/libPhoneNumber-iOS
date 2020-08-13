4377b21c870e9b41ca10b4798f5252d2accd571a
Jagdeep Manik <jagdeep.manik@berkeley.edu>
2020-08-12 19:28:41 -0700

    Fix 'umbrella header for module does not include...' error

--- libPhoneNumber-iOS/libPhoneNumberiOS.h
+++ libPhoneNumber-iOS/libPhoneNumberiOS.h
@@ -25,6 +25,7 @@ FOUNDATION_EXPORT const unsigned char libPhoneNumber_iOSVersionString[];
 
 // Metadata
 #import "NBMetadataHelper.h"
+#import "NBGeocoderMetadataHelper.h"
 
 // Model
 #import "NBNumberFormat.h"
