01f4051864a56620ecb64f94cd8609d0dd7f8dbd
Adam Lickel <adam@lickel.com>
2021-07-07 13:35:32 -0700

    Catalyst

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "libPhoneNumber",
+    platforms: [
+        .macOS(.v10_10),
+        .macCatalyst(.v13),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "libPhoneNumber",
+            targets: ["libPhoneNumber"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libPhoneNumber",
+            path: "libPhoneNumber",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreTelephony", .when(platforms: [.iOS, .macOS, .macCatalyst])),
+            ]
+        ),
+        .testTarget(
+            name: "libPhoneNumberTests",
+            dependencies: ["libPhoneNumber"],
+            path: "libPhoneNumberTests",
+            sources: [
+                "NBAsYouTypeFormatterTest.m",
+                "NBPhoneNumberParsingPerfTest.m",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.h",
+                "NBPhoneNumberUtil+ShortNumberTestHelper.m",
+                "NBPhoneNumberUtilTest.m",
+                "NBShortNumberInfoTest.m"
+            ]
+        )
+    ]
+)
