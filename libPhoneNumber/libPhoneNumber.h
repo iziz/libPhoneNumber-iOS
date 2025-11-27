03ef7a1b2eeb73081351ff02706cab58a4b3f078
Mathijs Bernson <mathijsb@q42.nl>
2025-11-27 17:36:35 +0100

    Add missing Foundation import

--- libPhoneNumber/libPhoneNumber.h
+++ libPhoneNumber/libPhoneNumber.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 ohtalk.me. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 //! Project version number for libPhoneNumber-iOS.
 FOUNDATION_EXPORT double libPhoneNumber_iOSVersionNumber;
 
